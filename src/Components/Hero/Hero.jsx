83cfbebba5be13322c131e51bc4beb1676e33449
Michael Abad <jun.abad@my.utsa.edu>
2024-09-14 22:05:18 -0500

    added banner and animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import './Hero.css';
 import profilePic from '../../images/profile-pic.jpg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import banner from '../../assets/junmike.dev-banner.jpeg';
 
 const Hero = () => {
   return (
     <div id="home" className="hero">
-      <img src={profilePic} alt="" />
-      <h1>
+      <img src={profilePic} id="profile-pic"alt="profile-pic" />
+      <img src={banner} id="banner" alt="banner" />
+
+      {/* <h1>
         <span>I'm Jun Michael,</span> Full Stack Software Engineer based in USA.
-      </h1>
+      </h1> */}
       <p>
       Hello! I'm a passionate Full Stack Software Engineer with a knack for building robust and scalable web applications. With a strong foundation in both front-end and back-end technologies, I bring a holistic approach to software development. Here&rsquo;s a glimpse into my skill set and experience:
       </p>
