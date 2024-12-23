e2d217f0d3dd6223f2dd44655febff91513bca8d
Michael Abad <jun.abad@my.utsa.edu>
2024-12-22 23:38:55 -0600

    fixed pic

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,9 +2,9 @@ import { useEffect, useRef } from 'react';
 import './Hero.css';
 import profilePic from '../../assets/hero_img.jpg';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import resumePDF from '../../assets/Michael Delacruz Abad Jr_Software Developer.pdf';
+// import resumePDF from '../../assets/Michael Delacruz Abad Jr_Software Developer.pdf';
 import githubSVG from '../../assets/github-mark.svg';
-import linkedInSVG from '../../assets/linkedin.svg';
+// import linkedInSVG from '../../assets/linkedin.svg';
 const Hero = () => {
   
   const heroActionRef = useRef(null);
